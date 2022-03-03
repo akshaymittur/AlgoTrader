517939a9c8da8ae549d332364387e951a068dfe5
Akshay <akshay.cs18@bmsce.ac.in>
2022-03-03 19:26:02 +0530

    Removed FnO

--- src/core/Algo.py
+++ src/core/Algo.py
@@ -6,36 +6,37 @@ from instruments.Instruments import Instruments
 from trademgmt.TradeManager import TradeManager
 
 from strategies.SampleStrategy import SampleStrategy
-from strategies.BNFORB30Min import BNFORB30Min
-from strategies.OptionSelling import OptionSelling
-from strategies.ShortStraddleBNF import ShortStraddleBNF
+# from strategies.BNFORB30Min import BNFORB30Min
+# from strategies.OptionSelling import OptionSelling
+# from strategies.ShortStraddleBNF import ShortStraddleBNF
 
 #from Test import Test
 
+
 class Algo:
-  isAlgoRunning = None
+    isAlgoRunning = None
 
-  @staticmethod
-  def startAlgo():
-    if Algo.isAlgoRunning == True:
-      logging.info("Algo has already started..")
-      return
-    
-    logging.info("Starting Algo...")
-    Instruments.fetchInstruments()
+    @staticmethod
+    def startAlgo():
+        if Algo.isAlgoRunning == True:
+            logging.info("Algo has already started..")
+            return
 
-    # start trade manager in a separate thread
-    tm = threading.Thread(target=TradeManager.run)
-    tm.start()
+        logging.info("Starting Algo...")
+        Instruments.fetchInstruments()
 
-    # sleep for 2 seconds for TradeManager to get initialized
-    time.sleep(2)
+        # start trade manager in a separate thread
+        tm = threading.Thread(target=TradeManager.run)
+        tm.start()
 
-    # start running strategies: Run each strategy in a separate thread
-    #threading.Thread(target=SampleStrategy.getInstance().run).start()
-    #threading.Thread(target=BNFORB30Min.getInstance().run).start()
-    #threading.Thread(target=OptionSelling.getInstance().run).start()
-    threading.Thread(target=ShortStraddleBNF.getInstance().run).start()
-    
-    Algo.isAlgoRunning = True
-    logging.info("Algo started.")
+        # sleep for 2 seconds for TradeManager to get initialized
+        time.sleep(2)
+
+        # start running strategies: Run each strategy in a separate thread
+        # threading.Thread(target=SampleStrategy.getInstance().run).start()
+        # threading.Thread(target=BNFORB30Min.getInstance().run).start()
+        # threading.Thread(target=OptionSelling.getInstance().run).start()
+        threading.Thread(target=ShortStraddleBNF.getInstance().run).start()
+
+        Algo.isAlgoRunning = True
+        logging.info("Algo started.")
