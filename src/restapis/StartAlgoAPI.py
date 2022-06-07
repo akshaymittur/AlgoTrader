029790030ff45002756192306977ad86c69f775c
Akshay <akshay.cs18@bmsce.ac.in>
2022-06-07 19:23:25 +0530

    Added market close to frontend

--- src/restapis/StartAlgoAPI.py
+++ src/restapis/StartAlgoAPI.py
@@ -4,15 +4,18 @@ import logging
 import threading
 from config.Config import getSystemConfig
 from core.Algo import Algo
+from utils.Utils import Utils
+
 
 class StartAlgoAPI(MethodView):
-  def post(self):
-    # start algo in a separate thread
-    x = threading.Thread(target=Algo.startAlgo)
-    x.start()
-    systemConfig = getSystemConfig()
-    homeUrl = systemConfig['homeUrl'] + '?algoStarted=true'
-    logging.info('Sending redirect url %s in response', homeUrl)
-    respData = { 'redirect': homeUrl }
-    return json.dumps(respData)
-  
+    def post(self):
+        # start algo in a separate thread
+        if Utils.isMarketClosedForTheDay():
+            return json.dumps({'error': "market closed"})
+        x = threading.Thread(target=Algo.startAlgo)
+        x.start()
+        systemConfig = getSystemConfig()
+        homeUrl = systemConfig['homeUrl'] + '?algoStarted=true'
+        logging.info('Sending redirect url %s in response', homeUrl)
+        respData = {'redirect': homeUrl}
+        return json.dumps(respData)
